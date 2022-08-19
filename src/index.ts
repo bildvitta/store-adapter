c151dceec90acaa223c98d4c83aca79c644d8cce
Douglas Calora <douglascalora123@gmail.com>
2022-08-19 10:48:25 -0300

    Releasing v1.0.0-beta.3

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { default as DefineGlobalPiniaStore } from './plugins/define-global-pinia-store'
+
+export { default as getActionPayload } from './helpers/get-action-payload'
 export { default as getState } from './helpers/get-state'
+export * from './helpers/store-handler'
